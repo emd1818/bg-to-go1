af7ed2945dbce7878ce4e2a89f38fa5c791cda61
emd1818 <emd@abv.bg>
2026-08-16 21:59:09 +0300

    Initialize main.jsx with React StrictMode

---
+++ src/main.jsx
@@ -0,0 +1,1 @@
+import { StrictMode } from "react"; import { createRoot } from "react-dom/client"; import BGtoGo from "./App.jsx"; createRoot(document.getElementById("root")).render( <StrictMode> <BGtoGo /> </StrictMode> );
